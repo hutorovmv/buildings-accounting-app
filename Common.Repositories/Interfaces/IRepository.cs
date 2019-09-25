80dec445f5c90c14434354ef3bd32510d9d9fca9
Mykola <hutorovmv@gmail.com>
2019-09-25 18:19:34 +0300

    Added common repository features

---
+++ Common.Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Common.Repositories.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        T Get(int id);
+        IEnumerable<T> GetAll();
+        void Create(T item);
+        void Update(T item);
+        void Delete(T item);
+    }
+}
---
+++ Common.Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Common.Repositories.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        void Save();
+    }
+}
