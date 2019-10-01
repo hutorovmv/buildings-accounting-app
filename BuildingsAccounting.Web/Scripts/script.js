b2bc39c59bed2eea37e8da3255dfe9b87fcb95dd
Mykola <hutorovmv@gmail.com>
2019-10-01 12:33:03 +0300

    Added photos carousel for building

---
+++ BuildingsAccounting.Web/Scripts/script.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: true,
+        navText: [
+            '<div class="cntrl bg-light rounded mx-1 p-3"><i class="fas fa-chevron-left text-dark"></i></div>',
+            '<div class="cntrl bg-light rounded mx-1 p-3"><i class="fas fa-chevron-right text-dark"></i></div>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            }
+        }
+    });
+
+    $("#add-file-btn").click(function () {
+        var input = '<div><input name="files" type="file" class="form-control-file my-2 w-100" /><i class="minus btn btn-sm btn-outline-info fas fa-minus"></i></div>';
+        $(input).appendTo("#add-files");
+    });
+
+    $(document).on("click", ".minus", function () {
+        $(this).parent('div').remove();
+    });
+});
